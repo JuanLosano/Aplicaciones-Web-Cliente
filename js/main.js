c01ec849015f9935b60ae41f45a4865fbc5161ed
Juan Losano <juanmlosano1@gmail.com>
2026-06-21 20:10:25 -0300

    Agregue logica de js y archivos svg

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
+//Variable que guarda los productos para las cards
+let productos = [];
+
+
+//Llamar a los elementos del DOM
+const contenedorCatalogo = document.querySelector('.catalogo');
+
+
+//Cargar los productos desde el JSON
+async function cargarProductos() {
+    try {
+        const response = await fetch('data/productos.json');
+        productos = await response.json(); // Es similar a parse .
+        renderizarCatalogo();
+    } catch (error) {
+        console.error('Error al cargar los productos:', error);
+    }
+}
+
+
+//Función para renderizar las cards de los productos
+function renderizarCatalogo() {
+contenedorCatalogo.innerHTML = ''; // Limpiar el contenedor antes de renderizar
+    const catalogo = productos.map( p => `
+        <article class="card">
+            <img src="${p.img}" alt="${p.alt}">
+            <h2>${p.titulo}</h2>
+            <p>Precio: $${p.precio}</p>
+            <button>Añadir al Carrito</button>
+        </article>
+        ` ).join('');
+    contenedorCatalogo.innerHTML = catalogo;
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await cargarProductos();
+});
+
+
+
+
+
+
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.menu-btn');
-const closeMenu = document.querySelector('.fantasma');
 
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
@@ -13,10 +55,3 @@ navToggle.addEventListener('click', () => {
     }
 });
 
-closeMenu.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute('data-visible');
-    if (visibility === "true") {
-        primaryNav.setAttribute('data-visible', "false");
-        navToggle.setAttribute('aria-expanded', "false");
-    }
-});
