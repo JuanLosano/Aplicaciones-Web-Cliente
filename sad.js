808354e046900a1376080ebe02512b0242cf6542
Juan Losano <juanmlosano1@gmail.com>
2026-06-27 16:42:43 -0300

    agregue logica de favoritos a la pagina

--- sad.js
+++ sad.js
@@ -24,3 +24,39 @@ closeMenu.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', "false");
     }
 });
+
+
+
+//Función para renderizar las cards de los productos
+function renderizarCatalogo() {
+contenedorCatalogo.innerHTML = ''; // Limpiar el contenedor antes de renderizar
+    const catalogo = productos.map( p => `
+        <article class="card">
+            <img src="${p.img}" alt="${p.alt}">
+            <h2>${p.titulo}</h2>
+            <p>Precio: $${p.precio}</p>
+            <button class="btn">Añadir al Carrito</button>
+            <button class="btn">Añadir a Favoritos</button>
+        </article>
+        ` ).join('');
+    contenedorCatalogo.innerHTML = catalogo;
+}
+
+
+
+// toggle de menu 
+
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.menu-btn');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', "true");
+        navToggle.setAttribute('aria-expanded', "true");
+    } else {
+        primaryNav.setAttribute('data-visible', "false");
+        navToggle.setAttribute('aria-expanded', "false");
+    }
+});
+
